711fed1c920ab4ad619e0f45c9cf802bd53c00c2
Faiza Husain and Qianqian Hu <qianqian@>
2018-02-05 11:47:18 -0800

    First version of C# program

---
+++ Animal.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class Animal
+{
+  public string Name;
+  public int Age;
+  public string Species;
+}
+
+public class Adoption
+{
+  public static void Main()
+  {
+    Animal dog = new Animal();
+    dog.Name = "Gou";
+    dog.Age = 1;
+    dog.Species = "Old English Sheepdog";
+
+    Animal cat = new Animal();
+    cat.Name = "Mao";
+    cat.Age = 1;
+    cat.Species = "Scottish Fold";
+
+    Animal panda = new Animal();
+    panda.Name = "Xiongmao";
+    panda.Age = 2;
+    panda.Species = "Bear";
+
+    Animal sloth = new Animal();
+    sloth.Name = "Shuta";
+    sloth.Age = 3;
+    sloth.Species = "Three Toes Sloth";
+
+    List<Animal> AdoptionAgency = new List<Animal>() { dog, cat, panda, sloth };
+
+    foreach(Animal pet in AdoptionAgency)
+    {
+      Console.WriteLine("Name: " + pet.Name);
+      Console.WriteLine("Age: " + pet.Age);
+      Console.WriteLine("Species: " + pet.Species);
+    }
+
+  }
+}
